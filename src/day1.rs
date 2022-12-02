534a665fe60765b7e5d1eb346354fcf105e4ba49
th3ed <ed4ndrews@gmail.com>
2022-12-01 23:55:27 -0500

    day 1 pt1/2 solutions:

--- src/day1.rs
+++ src/day1.rs
@@ -1,8 +1,8 @@
 use std::collections::BinaryHeap;
 
 #[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Vec<i32> {
-    let mut elves = Vec::new();
+pub fn input_generator(input: &str) -> BinaryHeap<i32> {
+    let mut elves: BinaryHeap<i32> = BinaryHeap::new();
     let mut stack: Vec<i32> = Vec::new();
     for line in input.lines() {
         if line == "" {
@@ -19,12 +19,17 @@ pub fn input_generator(input: &str) -> Vec<i32> {
 }
 
 #[aoc(day1, part1)]
-pub fn solve_part1(input: &Vec<i32>) -> i32 {
-    *input.iter().max().unwrap()
+pub fn solve_part1(input: &BinaryHeap<i32>) -> i32 {
+    *input.peek().unwrap()
+    // *input.iter().max().unwrap()
 }
 
 #[aoc(day1, part2)]
-pub fn solve_part2(input: &Vec<i32>) -> i32 {
-    input.select_nth_unstable(index)
+pub fn solve_part2(input: &BinaryHeap<i32>) -> i32 {
+    let mut heap = input.clone();
+    let mut out = 0;
+    for _ in 0..3 {
+        out += heap.pop().unwrap();
+    }
+    out
 }
-
